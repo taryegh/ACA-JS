8c5fe618dc89c741110f15696a87ba17ad4ce534
Taron Yeghoyan <49352957+taryegh@users.noreply.github.com>
2019-05-10 10:07:24 +0400

    Update homework5.js

--- homework5.js
+++ homework5.js
@@ -15,3 +15,32 @@ function removeFirstEl(array) {
 console.log(removeFirstEl([6, 78, 'n', 0, 1]));
 console.log(removeFirstEl([5]));
 console.log(removeFirstEl([]));
+
+
+
+
+
+
+// 2. Given an array of nested arrays. Write a recursive function that flattens it. (Hint create function that concats arrays).
+function flatenArr(array) {
+    if (array.length <= 1) {
+        if (Array.isArray(array[0])) {
+            return flatenArr(array[0]);
+        } else {
+            return array;
+        }
+    }
+
+
+    let firstEl = array.splice(0, 1);
+
+    if (Array.isArray(firstEl[0])) {
+        array = flatenArr(firstEl).concat(flatenArr(array));
+    } else {
+        array = firstEl.concat(flatenArr(array));
+    }
+
+    return array;
+}
+console.log(flatenArr([1, [3, 4, [1, 2]], 10]));
+console.log(flatenArr([14, [1, [[[3, []]], 1], 0]]));
