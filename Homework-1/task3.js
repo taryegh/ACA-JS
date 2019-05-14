eaf7a060cac8332a52fb684bea1a23cd7022276c
Taron Yeghoyan <49352957+taryegh@users.noreply.github.com>
2019-05-14 11:52:38 +0400

    Create task3.js

---
+++ Homework-1/task3.js
@@ -0,0 +1,20 @@
+'use strict'
+
+
+// 3. Given number ​n​ (positive integer). Print the value of ​n + nn + nnn​(not multiplication)​.
+function printNs(n) {
+    return parseInt(`${n}`) + parseInt(`${n}${n}`) + parseInt(`${n}${n}${n}`);
+
+}
+
+console.log(printNs(3));
+console.log(printNs(17));
+console.log(printNs(100));
+
+
+// parsing int with unary operator
+
+function printNs(n) {
+    return +(`${n}`) + +(`${n}${n}`) + +(`${n}${n}${n}`);
+
+}
