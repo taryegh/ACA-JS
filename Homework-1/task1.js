ef0d9cdc2c4bd59eb5da3311eaa791bd4667690f
Taron Yeghoyan <49352957+taryegh@users.noreply.github.com>
2019-05-14 11:36:40 +0400

    Create task1.js

---
+++ Homework-1/task1.js
@@ -0,0 +1,26 @@
+
+'use strict'
+
+
+// 1. Given a number. Print “odd” if the number is odd and “even” if it’s even.
+function checkOdd_Even(number) {
+    if (number % 2 === 0) {
+        console.log('The number is even');
+    } else {
+        console.log('The number is odd');
+    }
+}
+checkOdd_Even(123);
+checkOdd_Even(35);
+checkOdd_Even(70);
+
+
+
+// Solving with ternary operator
+
+function checkOdd_EvenTer(n) {
+    return n % 2 === 0 ? 'even' : 'odd';
+}
+console.log(checkOdd_EvenTer(123));
+console.log(checkOdd_EvenTer(35));
+console.log(checkOdd_EvenTer(70));
