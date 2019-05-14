945ce9bb77f1143b1a77e3cbdf7f7f80144ca161
Taron Yeghoyan <49352957+taryegh@users.noreply.github.com>
2019-05-14 12:34:04 +0400

    Create task5.js

---
+++ Homework-1/task5.js
@@ -0,0 +1,11 @@
+'use strict'
+
+
+// 5. Given five numbers as input. Calculate and print the average of the numbers(without using arrays).
+
+function calcAvg(a1, a2, a3, a4, a5) {
+    return (a1 + a2 + a3 + a4 + a5) / 5;
+}
+
+console.log(calcAvg(45, -12, 0, 3, -15));
+console.log(calcAvg(7, 52, -23, 9, -81));
