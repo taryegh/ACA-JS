e755ed5b15bf8dafc83b1927c8a237d01578ecc6
Taron Yeghoyan <49352957+taryegh@users.noreply.github.com>
2019-05-16 09:04:15 +0400

    Create task_03.js

---
+++ Homework-2/task_03.js
@@ -0,0 +1,19 @@
+'use strict'
+
+
+// 3. Given a number n( n> 0 ). Print Fibonacci series up to n.
+function fibSeries(n) {
+    let arr = [0, 1];
+
+    for (let i = 2; i <= n; i++) {
+        let res = arr[i - 2] + arr[i - 1];
+
+        if (res <= n) {
+            arr.push(res);
+        }
+
+    }
+    return arr;
+}
+console.log(fibSeries(7));
+console.log(fibSeries(45));
