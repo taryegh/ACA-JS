b95a936ab6c0ed441026b7508cd6b17da218a6c4
Taron Yeghoyan <49352957+taryegh@users.noreply.github.com>
2019-05-17 19:09:28 +0400

    Create task_05.js

---
+++ Classworks/Classwork_01/task_05.js
@@ -0,0 +1,12 @@
+'use strict'
+
+
+// 5. Check whether a given number is negative. Print “yes”, if it is negative, print “no” otherwise.
+
+function checkNeg(n) {
+    return n < 0 ? 'Yes' : 'No';
+}
+
+console.log(checkNeg(0));
+console.log(checkNeg(-71));
+console.log(checkNeg(89));
