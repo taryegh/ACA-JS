de33f207f50ff73ec1c8e8f3320827e65ce2936f
Taron Yeghoyan <49352957+taryegh@users.noreply.github.com>
2019-05-17 19:13:46 +0400

    Create task_11.js

---
+++ Classworks/Classwork_01/task_11.js
@@ -0,0 +1,9 @@
+// 11. Given a number. Print its absolute value.
+
+function printAbs(n) {
+    return Math.abs(n);
+}
+
+console.log(printAbs(-123))
+console.log(printAbs(35))
+console.log(printAbs(0));
