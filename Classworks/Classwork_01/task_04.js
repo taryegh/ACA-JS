3e001cc6763189b16221b13371ce5cb5bd37875e
Taron Yeghoyan <49352957+taryegh@users.noreply.github.com>
2019-05-17 19:02:02 +0400

    Create task_04.js

---
+++ Classworks/Classwork_01/task_04.js
@@ -0,0 +1,13 @@
+'use strict'
+
+
+// 4. Declare a variable with defined value. Print the last digit of the number. 
+
+function printLDig(n) {
+    return n % 10;
+}
+
+console.log(printLDig(78756));
+console.log(printLDig(-122));
+console.log(printLDig(8));
+console.log(printLDig(0));
