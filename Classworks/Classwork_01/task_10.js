a43183e4b123df2d88d2d3890259400387c8965f
Taron Yeghoyan <49352957+taryegh@users.noreply.github.com>
2019-05-17 19:13:20 +0400

    Create task_10.js

---
+++ Classworks/Classwork_01/task_10.js
@@ -0,0 +1,9 @@
+// 10. Given three numbers. Find the maximum one.
+
+function findMax(a, b, c) {
+    return Math.max(a, b, c);
+}
+
+console.log(findMax(-4 , 6, 0));
+console.log(findMax(1020, 500, - 450));
+console.log(findMax(14, 8, 14));
