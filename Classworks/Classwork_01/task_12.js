4f91b8251d6067019d16af63410d4bf649ee3d15
Taron Yeghoyan <49352957+taryegh@users.noreply.github.com>
2019-05-17 19:14:08 +0400

    Create task_12.js

---
+++ Classworks/Classwork_01/task_12.js
@@ -0,0 +1,9 @@
+// 12. Given a number. Print “odd” if the number is odd and “even” if it’s even.	
+
+function printOddEv(n) {
+    return n % 2 ? 'Odd' : 'Even';
+}
+
+console.log(printOddEv(123));
+console.log(printOddEv(35));
+console.log(printOddEv(70));
