6033e9f9177eaa3d1e023562631903a018b14c9e
Taron Yeghoyan <49352957+taryegh@users.noreply.github.com>
2019-05-17 18:57:04 +0400

    Create task_02.js

---
+++ Classworks/Classwork_01/task_02.js
@@ -0,0 +1,10 @@
+'use strict'
+
+
+// 2. Print your name and age in the following format: “My name is ____, I am ____ .”
+
+function printCr(name, age) {
+    return `My name is ${name}, I am ${age}.`
+}
+
+console.log(printCr('Jack', 25));
