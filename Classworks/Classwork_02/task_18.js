a7c067d5a570dc072b3180dca69d5aad210d5044
Taron Yeghoyan <49352957+taryegh@users.noreply.github.com>
2019-05-19 09:41:32 +0400

    Create task_18.js

---
+++ Classworks/Classwork_02/task_18.js
@@ -0,0 +1,20 @@
+'use strict'
+
+
+// 18. Given a number n ( n>= 0 ). Print nth Fibonacci number. (Fibonacci series: 0, 1, 1, 2, 3, 5, 8 …, ak = ak-1 + ak-2)
+
+function fibN(n) {
+    let arr = [0, 1];
+    
+    for (let i = 2; i <= n; i++) {
+        let res = arr[i - 2] + arr[i - 1];
+        arr.push(res);
+    }
+    
+    return arr[n];
+}
+
+console.log(fibN(0));
+console.log(fibN(2));
+console.log(fibN(10));
+console.log(fibN(20));
