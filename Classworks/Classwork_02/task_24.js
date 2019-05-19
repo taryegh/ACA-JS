e5dadc61f7b8bf6d847201e036a96b5e7e9b26a9
Taron Yeghoyan <49352957+taryegh@users.noreply.github.com>
2019-05-19 09:55:52 +0400

    Create task_24.js

---
+++ Classworks/Classwork_02/task_24.js
@@ -0,0 +1,25 @@
+'use strict'
+
+
+// 24. Write a program to print X star pattern series using loop.
+
+function printX(n) {
+    let res = '';
+
+    for (let i = 0; i < n; i++) {
+
+        for (let j = 0; j < n; j++) {
+            if (i === j || n - i - 1 === j) {
+                res += '* ';
+            } else {
+                res += '  ';
+            }
+        }
+
+        res += '\n'
+    }
+    
+    return res;
+}
+
+console.log(printX(7));
