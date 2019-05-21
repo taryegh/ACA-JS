96180bf1bcf1d7b0d992369cd9e37fe80c37b60e
Taron Yeghoyan <49352957+taryegh@users.noreply.github.com>
2019-05-21 17:30:28 +0400

    Update task_03.js

--- Homework_01/task_03.js
+++ Homework_01/task_03.js
@@ -3,8 +3,7 @@
 
 // 3. Given number ​n​ (positive integer). Print the value of ​n + nn + nnn​(not multiplication)​.
 function printNs(n) {
-    return parseInt(`${n}`) + parseInt(`${n}${n}`) + parseInt(`${n}${n}${n}`);
-
+  return parseInt(`${n}`) + parseInt(`${n}${n}`) + parseInt(`${n}${n}${n}`);
 }
 
 console.log(printNs(3));
@@ -14,7 +13,10 @@ console.log(printNs(100));
 
 // parsing int with unary operator
 
-function printNs(n) {
-    return +(`${n}`) + +(`${n}${n}`) + +(`${n}${n}${n}`);
-
+function printNs2(n) {
+  return +(`${n}`) + +(`${n}${n}`) + +(`${n}${n}${n}`);
 }
+
+console.log(printNs2(3));
+console.log(printNs2(17));
+console.log(printNs2(100));
