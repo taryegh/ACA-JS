2a7b3c82a8872f5bcac7ea42fbfe6b55be3d1355
Taron Yeghoyan <49352957+taryegh@users.noreply.github.com>
2019-05-21 11:32:55 +0400

    Update task_01.js

--- Homework_07/task_01.js
+++ Homework_07/task_01.js
@@ -6,44 +6,44 @@
 // It should have appropriate getters and setters. It should have a toString method.
 
 class Author {
-	constructor(name, email, gender) {
-		this.name = name;
-		this.email = email;
-		this.gender = gender;
-	}
+  constructor(name, email, gender) {
+    this.name = name;
+    this.email = email;
+    this.gender = gender;
+  }
 
-	get name() {
-		return this._name;
-	}
-	set name(value) {
-		this._name = value;
-	}
+  get name() {
+    return this._name;
+  }
 
-	get email() {
-		return this._email;
-	}
-	set email(value) {
-		this._email = value;
-	}
+  set name(value) {
+    this._name = value;
+  }
 
-	get gender() {
-		return this._gender;
-	}
-	set gender(value) {
-		this._gender = value;
-	}
+  get email() {
+    return this._email;
+  }
+
+  set email(value) {
+    this._email = value;
+  }
+
+  get gender() {
+    return this._gender;
+  }
+
+  set gender(value) {
+    this._gender = value;
+  }
 
 
-	toString() {
-		return `name: ${this.name}, eMail: ${this.email}, gender: ${this.gender}`;
-	}
+  toString() {
+    return `name: ${this.name}, eMail: ${this.email}, gender: ${this.gender}`;
+  }
 }
 
-let author01 = new Author('John Hammond', 'jhammond@jurassic.com', 'male');
+const author01 = new Author('John Hammond', 'jhammond@jurassic.com', 'male');
 console.log(author01);
-
-
-
 
 
 // 1.2 Create Book class.
@@ -53,50 +53,54 @@ console.log(author01);
 // It should have a toString method.
 
 class Book {
-	constructor(title, author, price, quantity) {
-		this.title = title;
-		this.author = author;
-		this.price = price;
-		this.quantity = quantity;
-	}
+  constructor(title, author, price, quantity) {
+    this.title = title;
+    this.author = author;
+    this.price = price;
+    this.quantity = quantity;
+  }
 
-	get title() {
-		return this._title;
-	}
-	set title(value) {
-		this._title = value;
-	}
+  get title() {
+    return this._title;
+  }
 
-	get author() {
-		return this._author;
-	}
-	set author(value) {
-		this._author = value;
-	}
+  set title(value) {
+    this._title = value;
+  }
 
-	get price() {
-		return this._price;
-	}
-	set price(value) {
-		this._price = value;
-	}
+  get author() {
+    return this._author;
+  }
 
-	get quantity() {
-		return this._quantity;
-	}
-	set quantity(value) {
-		this._quantity = value;
-	}
+  set author(value) {
+    this._author = value;
+  }
+
+  get price() {
+    return this._price;
+  }
+
+  set price(value) {
+    this._price = value;
+  }
+
+  get quantity() {
+    return this._quantity;
+  }
+
+  set quantity(value) {
+    this._quantity = value;
+  }
 
 
-	getProfit() {
-		return `Profit: $${this.price * this.quantity}`;
-	}
+  getProfit() {
+    return `Profit: $${this.price * this.quantity}`;
+  }
 
-	toString() {
-		return `Title: ${this.title}, Author: ${this.author}, Price: ${this.price}, Quantity: ${this.quantity}`;
-	}
+  toString() {
+    return `Title: ${this.title}, Author: ${this.author}, Price: ${this.price}, Quantity: ${this.quantity}`;
+  }
 }
 
-let book01 = new Book('Dead Mountaineer\'s Hotel', 'Arkady and Boris Strugatsky', 10, 200000);
+const book01 = new Book('Dead Mountaineer\'s Hotel', 'Arkady and Boris Strugatsky', 10, 200000);
 console.log(book01);
