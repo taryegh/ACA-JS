368efe0b6564602c722042c07405ff8426d422dc
Taron Yeghoyan <49352957+taryegh@users.noreply.github.com>
2019-05-21 17:23:45 +0400

    Update task_02.js

--- Homework_01/task_02.js
+++ Homework_01/task_02.js
@@ -4,24 +4,21 @@
 // 2. Given two numbers. Print 1 if one of them is divisible by the other one, otherwise print 0.
 
 function checkIsDevisable(a, b) {
-    if ((a % b === 0) || (b % a === 0)) {
-        console.log(1);
-    } else {
-        console.log(0);
-    }
+  if ((a % b === 0) || (b % a === 0)) {
+    return 1;
+  }
+  return 0;
 }
 
-checkIsDevisable(3, 14);
-checkIsDevisable(18, 2);
-checkIsDevisable(7, 21);
-
-
+console.log(checkIsDevisable(3, 14));
+console.log(checkIsDevisable(18, 2));
+console.log(checkIsDevisable(7, 21));
 
 
 // Solving with ternary operator
 
 function checkIsDevisableTern(a, b) {
-    return a % b === 0 || b % a === 0 ? 1 : 0;
+  return a % b === 0 || b % a === 0 ? 1 : 0;
 }
 
 console.log(checkIsDevisableTern(3, 14));
