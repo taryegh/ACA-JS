75bdd50786b1d26aff6b6ed44afdd5d377ac604e
Taron Yeghoyan <49352957+taryegh@users.noreply.github.com>
2019-05-21 23:40:05 +0400

    Update task_04.js

--- Homework_01/task_04.js
+++ Homework_01/task_04.js
@@ -4,19 +4,18 @@
 // 4. Given a positive integer. Bring the last digit of the number to the beginning. Print the new number. If the last digit of the inserted number is 0, number remains the same.
 
 function bringLastDig(n) {
-    if (n < 0 || n === undefined) {
-        return 'Please enter positive number';
-    }
+  if (n < 0 || n === undefined) {
+    return 'Please enter positive number';
+  }
 
-    let nToStr = n.toString();
+  const nToStr = n.toString();
 
-    if (nToStr[nToStr.length - 1] !== '0') {
-        let lEl = nToStr.slice(nToStr.length - 1);
-        let rest = nToStr.slice(0, nToStr.length - 1);
-        return parseInt((lEl + rest));
-    } else {
-        return n;
-    }
+  if (nToStr[nToStr.length - 1] !== '0') {
+    const lEl = nToStr.slice(nToStr.length - 1);
+    const rest = nToStr.slice(0, nToStr.length - 1);
+    return parseInt((lEl + rest));
+  }
+  return n;
 }
 
 console.log(bringLastDig(736));
@@ -25,19 +24,17 @@ console.log(bringLastDig(250));
 console.log(bringLastDig(8));
 
 
-
-
 // Other method
 
 function lastDig(n) {
-    if (n < 0 || n === undefined) {
-        return 'Please enter a positive number';
-    }
+  if (n < 0 || n === undefined) {
+    return 'Please enter a positive number';
+  }
 
-    const lDig = n % 10;
-    let rest = Math.floor(n / 10);
-    
-    return lDig !== 0 && rest !== 0 ? +lDig.toString() + rest.toString() : n;
+  const lDig = n % 10;
+  const rest = Math.floor(n / 10);
+
+  return lDig !== 0 && rest !== 0 ? +lDig.toString() + rest.toString() : n;
 }
 
 console.log(lastDig(367));
