c6ba2f14bfa992a55c8122e0ca74ff3a680d4b88
Taron Yeghoyan <49352957+taryegh@users.noreply.github.com>
2019-05-22 14:15:26 +0400

    Add files via upload

---
+++ Homework_01/task_06.js
@@ -0,0 +1,46 @@
+'use strict';
+
+
+// 6. Given three numbers. Sort them by the ascending order
+
+function sortNum(n1, n2, n3) {
+  if (n1 < n2 && n1 < n3) {
+    if (n2 <= n3) {
+      console.log(`${n1}, ${n2}, ${n3}`);
+    } else {
+      console.log(`${n1}, ${n3}, ${n2}`);
+    }
+  } else if (n2 < n3 && n2 < n3) {
+    if (n3 <= n1) {
+      console.log(`${n2}, ${n3}, ${n1}`);
+    } else {
+      console.log(`${n2}, ${n1}, ${n3}`);
+    }
+  } else if (n2 <= n3) {
+    console.log(`${n3}, ${n2}, ${n1}`);
+  } else {
+    console.log(`${n3}, ${n1}, ${n2}`);
+  }
+}
+
+sortNum(45, 26, 78);
+sortNum(-23, -456, 0);
+
+
+// another method
+function sortN(...args) {
+  return args.sort((a, b) => a - b);
+}
+
+console.log(sortN(45, 26, 78));
+console.log(sortN(-23, -456, 0));
+
+
+// another method
+function sortNum2(n1, n2, n3) {
+  const arr = [n1, n2, n3];
+  return arr.sort((a, b) => a - b);
+}
+
+console.log(sortNum2(45, 26, 78));
+console.log(sortNum2(-23, -456, 0));
