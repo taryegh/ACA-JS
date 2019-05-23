1ba7523cebb008f50ce86f9fe3b336b816ec909f
Taron Yeghoyan <49352957+taryegh@users.noreply.github.com>
2019-05-23 07:51:36 +0400

    Add files via upload

---
+++ Homeworks/Homework_02/task_10.js
@@ -0,0 +1,36 @@
+'use strict';
+
+
+// 10. Given an array of numbers. Find the index of the second maximum element.
+
+function findSecondMax(arr) {
+  let biggest = -Infinity; let nextBiggest = -Infinity;
+
+  for (let i = 0; i < arr.length; i++) {
+    const cur = arr[i];
+
+    if (cur > biggest) {
+      nextBiggest = biggest;
+      biggest = cur;
+    } else if (cur < biggest && cur > nextBiggest) {
+      nextBiggest = cur;
+    }
+  }
+
+  return arr.indexOf(nextBiggest);
+}
+
+console.log(findSecondMax([23, -98, 0, -456, 12, 8]));
+console.log(findSecondMax([-60, 2, 43, -18, 5, -19, 36, 7, 56]));
+
+
+// another method
+function SecMax(arr) {
+  const sArr = arr.slice().sort((a, b) => a - b);
+  sArr.pop();
+  const lEl = sArr.pop();
+  return arr.indexOf(lEl);
+}
+
+console.log(SecMax([23, -98, 0, -456, 12, 8]));
+console.log(SecMax([-60, 2, 43, -18, 5, -19, 36, 7, 56]));
