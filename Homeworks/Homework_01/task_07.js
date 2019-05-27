4543ed328e4c0c64a70daf3fba7bcc995322c626
Taron Yeghoyan <49352957+taryegh@users.noreply.github.com>
2019-05-28 01:37:46 +0400

    Update task_07.js

--- Homeworks/Homework_01/task_07.js
+++ Homeworks/Homework_01/task_07.js
@@ -1,37 +1,9 @@
 'use strict';
 
 
-// 7. Find the sign of product of three numbers ​without ​multiplication operator. Display the specified sign.
+// 7. Find the sign of product of three numbers without multiplication operator. Display the specified sign.
 
-
-function checkSign(a, b, c) {
-  if (a === 0 || b === 0 || c === 0) {
-    return 'unsigned';
-  }
-
-  const arr = [a, b, c];
-  let count = 0;
-
-  for (let i = 0; i < arr.length; i++) {
-    if (arr[i] < 0) {
-      count += 1;
-    }
-  }
-
-  if (count % 2 === 0) {
-    return '+';
-  }
-
-  return '-';
-}
-
-console.log(checkSign(-14, 5, 0));
-console.log(checkSign(-8, 9, -6));
-console.log(checkSign(4, 19, -2));
-
-
-// 7. Another method
-function checkSign2(...nums) {
+function checkSign(...nums) {
   if (nums.indexOf(0) > -1) {
     return 'unsigned';
   }
@@ -47,6 +19,6 @@ function checkSign2(...nums) {
   return count % 2 ? '-' : '+';
 }
 
-console.log(checkSign2(-14, 5, 0));
-console.log(checkSign2(-8, 9, -6));
-console.log(checkSign2(4, 19, -2));
+console.log(checkSign(-14, 5, 0));
+console.log(checkSign(-8, 9, -6));
+console.log(checkSign(4, 19, -2));
