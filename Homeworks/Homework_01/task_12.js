120513dbb48f10d26f27800c783e4b7b7699452b
Taron Yeghoyan <49352957+taryegh@users.noreply.github.com>
2019-05-28 03:16:33 +0400

    Update task_12.js

--- Homeworks/Homework_01/task_12.js
+++ Homeworks/Homework_01/task_12.js
@@ -4,9 +4,9 @@
 // 12. Write a program which will compute the area of a rectangular or a triangle after prompting the user to type the name of the figure name. Also check that entered numbers are positive. For the triangle entered numbers are height and and base.
 
 function calcArea() {
-  const type = prompt('Please enter type');
-  const height = prompt('Please enter height');
-  const base = prompt('Please enter base');
+  const type = prompt('Enter type');
+  const height = prompt('Enter height');
+  const base = prompt('Enter base');
 
   if (height <= 0 || base <= 0) {
     return 'Please enter only positives';
@@ -14,7 +14,9 @@ function calcArea() {
 
   if (type === 'triangle') {
     return `Area of triangle is ${(height * base * 0.5)}`;
-  } else if (type === 'rectangle') {
+  }
+
+  if (type === 'rectangle') {
     return `Area of rectangle is ${(height * base)}`;
   }
 
