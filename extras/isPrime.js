349fe3bdbf2d5c3431264dece056b1a88a857193
Taron Yeghoyan <49352957+taryegh@users.noreply.github.com>
2019-05-27 18:26:55 +0400

    Update isPrime.js

--- extras/isPrime.js
+++ extras/isPrime.js
@@ -1,16 +1,43 @@
-function showPrimes(n) {
+'use strict';
 
+
+// 7. Write a JavaScript function that accepts a string as a parameter and counts the number of vowels within the string.
+
+function showPrimes(n) {
   for (let i = 2; i < n; i++) {
     if (!isPrime(i)) continue;
 
-    alert(i);
+    console.log(i);
   }
 }
 
 function isPrime(n) {
   for (let i = 2; i < n; i++) {
-    if (n % i == 0) return false;
+    if (n % i === 0) return false;
   }
 
   return true;
 }
+
+showPrimes(32);
+
+
+// another solution - check if the num is prime
+function testPrime(n) {
+  if (n === 1) {
+    return false;
+  }
+  if (n === 2) {
+    return true;
+  }
+
+  for (let i = 2; i < n; i++) {
+    if (n % i === 0) {
+      return false;
+    }
+  }
+
+  return true;
+}
+
+console.log(testPrime(37));
