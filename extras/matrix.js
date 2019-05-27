bd84b5c0595a9acb1d38b671a3eb0bb8a27e2a93
Taron Yeghoyan <49352957+taryegh@users.noreply.github.com>
2019-05-27 18:50:49 +0400

    Create matrix.js

---
+++ extras/matrix.js
@@ -0,0 +1,22 @@
+'use strict';
+
+
+// 10. Write a JavaScript function which returns the n rows by n columns identity matrix.
+
+function getMatrix(n) {
+  let res = '';
+
+  for (let i = 1; i <= n; i++) {
+    for (let j = 1; j <= n; j++) {
+      if (i === j) {
+        res += 1;
+      } else {
+        res += '0';
+      }
+    }
+    res += '\n';
+  }
+
+  return res;
+}
+console.log(getMatrix(5));
