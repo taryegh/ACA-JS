8a20f5d5065a4474019db96a96d25e87aa6c6e4e
Taron Yeghoyan <49352957+taryegh@users.noreply.github.com>
2019-05-27 22:09:01 +0400

    Update task_02.js

--- Homeworks/Homework_01/task_02.js
+++ Homeworks/Homework_01/task_02.js
@@ -3,24 +3,24 @@
 
 // 2. Given two numbers. Print 1 if one of them is divisible by the other one, otherwise print 0.
 
-function checkIsDev(a, b) {
+function isDiv(a, b) {
   if ((a % b === 0) || (b % a === 0)) {
     return 1;
   }
   return 0;
 }
 
-console.log(checkIsDev(3, 14));
-console.log(checkIsDev(18, 2));
-console.log(checkIsDev(7, 21));
+console.log(isDiv(3, 14));
+console.log(isDiv(18, 2));
+console.log(isDiv(7, 21));
 
 
 // Solving with ternary operator
 
-function checkIsDevT(a, b) {
+function isDivT(a, b) {
   return a % b === 0 || b % a === 0 ? 1 : 0;
 }
 
-console.log(checkIsDevT(3, 14));
-console.log(checkIsDevT(18, 2));
-console.log(checkIsDevT(7, 21));
+console.log(isDivT(3, 14));
+console.log(isDivT(18, 2));
+console.log(isDivT(7, 21));
