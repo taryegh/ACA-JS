f2e9b0544afa6b1037abf5d7eb07c36c0da9bfd1
Taron Yeghoyan <49352957+taryegh@users.noreply.github.com>
2019-05-27 22:06:54 +0400

    Update task_01.js

--- Homeworks/Homework_01/task_01.js
+++ Homeworks/Homework_01/task_01.js
@@ -3,24 +3,24 @@
 
 // 1. Given a number. Print “odd” if the number is odd and “even” if it’s even.
 
-function checkOddEven(n) {
+function isEven(n) {
   if (n % 2 === 0) {
-    return 'The number is even';
+    return 'even';
   }
-  return 'The number is odd';
+  return 'odd';
 }
 
-console.log(checkOddEven(123));
-console.log(checkOddEven(35));
-console.log(checkOddEven(70));
+console.log(isEven(123));
+console.log(isEven(35));
+console.log(isEven(70));
 
 
 // Solving with ternary operator
 
-function checkOddEvenT(n) {
+function isEvenT(n) {
   return n % 2 ? 'odd' : 'even';
 }
 
-console.log(checkOddEvenT(123));
-console.log(checkOddEvenT(35));
-console.log(checkOddEvenT(70));
+console.log(isEvenT(123));
+console.log(isEvenT(35));
+console.log(isEvenT(70));
