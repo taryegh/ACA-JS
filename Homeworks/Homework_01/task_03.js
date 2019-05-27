43b01d9b08a291808d19f8ece411926971127524
Taron Yeghoyan <49352957+taryegh@users.noreply.github.com>
2019-05-27 22:13:15 +0400

    Update task_03.js

--- Homeworks/Homework_01/task_03.js
+++ Homeworks/Homework_01/task_03.js
@@ -1,7 +1,7 @@
 'use strict';
 
 
-// 3. Given number ​n​ (positive integer). Print the value of ​n + nn + nnn​(not multiplication)​.
+// 3. Given number n (positive integer). Print the value of n + nn + nnn(not multiplication).
 
 function printNs(n) {
   return parseInt(`${n}`) + parseInt(`${n}${n}`) + parseInt(`${n}${n}${n}`);
