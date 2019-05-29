482c374b518e18ff36988ecee2e5c517c8550250
Taron Yeghoyan <49352957+taryegh@users.noreply.github.com>
2019-05-29 10:41:41 +0400

    Create random.js

---
+++ Exams/Exam_02/Prep/other/random.js
@@ -0,0 +1,94 @@
+
+
+1. isNan function returns ______ if the argument is not a number otherwise it is ______ 
+False/True 
+False/False 
+True / False 
+True / True 
+Answer: True / False 
+
+
+
+
+2. Code A: var x = 10; y = --x + 1; 
+alert(y); 
+Code B: var x = 10; 
+y = x-- + 1; 
+alert(y); 
+What is the output for code A and B? 
+10,11 
+10,10 
+11,10 
+11,11 
+Answer: 10,11 
+
+
+
+3. FIbonacci Series Program 
+function fibonacciSequence(input) {
+  var a = 0; b = 1; c = 0;
+  var array1 = [0, 1];
+  for (var i = 2; i <= input; i++) {
+    c = a + b;
+    a = b;
+    b = c;
+    array1.push(c)
+  }
+  return array1;
+} 
+
+Above code is true
+Above code is incorrect 
+Answer: True 
+
+
+
+
+4. What is the output for the following function
+function test(x) {
+  while (x < 5) { x++; }
+  return x;
+}
+alert(test(2));
+
+Answer: 5
+
+
+
+
+5. _____ is an function contained within an object 
+Object 
+None of the options 
+Function 
+Method 
+Answer: Method 
+
+
+
+
+
+6. Function in JavaScript is an object. True or False ? 
+False 
+True 
+Answer: True 
+
+
+
+
+
+
+7. What is the output of the following expression? 
+function multi(x, y) {
+  var c = x * y;
+}
+multi(20, 3); 
+
+20 
+60 
+3 
+Nothing 
+Answer:-Nothing 
+
+
+
+
