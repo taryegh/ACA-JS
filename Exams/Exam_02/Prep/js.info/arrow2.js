238aa855fe682a80b22a1a783a9d4020baeb3535
Taron Yeghoyan <49352957+taryegh@users.noreply.github.com>
2019-05-30 12:41:32 +0400

    Update arrow2.js

--- Exams/Exam_02/Prep/js.info/arrow2.js
+++ Exams/Exam_02/Prep/js.info/arrow2.js
@@ -11,7 +11,7 @@ let group = {
 
   showList() {
     this.students.forEach(
-      student => console.log(this.title + ': ' + student)
+      el => console.log(this.title + ': ' + el)
     );
   }
 };
