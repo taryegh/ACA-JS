e5c4773659d9eebb3e995d84275cbcb490dc6ecb
Taron Yeghoyan <49352957+taryegh@users.noreply.github.com>
2019-06-09 18:00:11 +0400

    Create script.js

---
+++ Practice/js.info/DOM/Walking DOM/script.js
@@ -0,0 +1,13 @@
+'use strict';
+
+// 1. How to access:
+// The < div > DOM node ?
+// The < ul > DOM node ?
+// The second<li>(with Pete)?
+
+
+
+console.log(document.body.firstElementChild);
+console.log(document.body.children[1]);
+console.log(document.body.children[1].children[1]);
+console.log(document.body.lastElementChild);
