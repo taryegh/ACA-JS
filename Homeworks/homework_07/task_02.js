31394a4cbf2cac6cb10aa4a6fbab8f2bab72de97
Taron Yeghoyan <49352957+taryegh@users.noreply.github.com>
2019-06-22 22:58:20 +0400

    Update task_02.js

--- Homeworks/homework_07/task_02.js
+++ Homeworks/homework_07/task_02.js
@@ -1,5 +1,15 @@
 'use strict';
 
+
+// Create an Account class. Account should have: id, name, balance.
+// It should have setters for name and balance, and getters for all fields.
+// It should have a method: credit(amount), which should add amount to balance and return the updated balance.
+// It should have a method: debit(amount), which should subtract the amount from the balance, if amount is less than the balance, otherwise output “Amount exceeded balance.”
+// It should have a method: transferTo(anotherAccount, amount): which should subtract the amount from the account balance and add it to the given anotherAccount and return the updated balance, if amount is less than the balance, otherwise output “Amount exceeded balance.”
+// It should have a static method: identifyAccounts(accountFirst, accountSecond) which gets two accounts and identifies if they are the same or not comparing all fields.
+// It should have toString method.
+
+
 class Account {
   constructor(id, name, balance) {
     this._id = id;
