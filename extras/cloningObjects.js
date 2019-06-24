0c098a0644581bd3a3d362b123e2488d17453ff7
Taron Yeghoyan <49352957+taryegh@users.noreply.github.com>
2019-06-24 09:30:47 +0400

    Create cloningObjects.js

---
+++ extras/cloningObjects.js
@@ -0,0 +1,38 @@
+'use strict';
+
+
+let obj = {
+  name: 'Geralt',
+  surname: 'of Riva',
+  nick: 'White Wolf',
+  gender: 'male',
+  profession: 'Witcher'
+}
+console.log(obj);
+
+
+// Cloning with for in loop
+let clone1 = {};
+
+for (let key in obj) {
+  clone1[key] = obj[key];
+}
+clone1.girlfriend = 'Yennefer';
+console.log(clone1);
+
+
+
+
+// Cloning with Object Assign
+let clone2 = Object.assign({}, obj);
+clone2.home = 'Kaer Morhen';
+console.log(clone2);
+
+
+
+
+// Cloning with JSON
+let objSt = JSON.stringify(obj);
+let clone3 = JSON.parse(objSt);
+clone3.hair = 'white';
+console.log(clone3);
