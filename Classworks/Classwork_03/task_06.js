58a4192ccb9898347b37fb044944d255ede64a38
Taron Yeghoyan <49352957+taryegh@users.noreply.github.com>
2019-08-14 16:46:35 +0400

    Create task_06.js

---
+++ Classworks/Classwork_03/task_06.js
@@ -0,0 +1,7 @@
+// 6.	Given an array of numbers. Find the maximum element in array.
+function findMax(arr) {
+  return Math.max(...arr);
+}
+
+console.log(findMax([1, 10, 2, 2, 3]));
+console.log(findMax([1, 4, 43, -112]));
