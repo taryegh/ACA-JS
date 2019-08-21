7cdb71a7d2937926b674a2363d102b5e8eab05a0
Taron Yeghoyan <taron_yeghoyan17@alumni.aua.am>
2019-08-21 17:37:21 +0400

    update

---
+++ extras/propertyDescriptor.js
@@ -0,0 +1,52 @@
+"use strict";
+
+// getOwnPropertyDescriptor
+let user = {
+  name: "John"
+};
+
+let descriptor = Object.getOwnPropertyDescriptor(user, "name");
+
+alert(JSON.stringify(descriptor, null, 2));
+/* property descriptor:
+{
+  "value": "John",
+  "writable": true,
+  "enumerable": true,
+  "configurable": true
+}
+*/
+
+// ---- ----
+
+// defineProperty
+let user = {};
+
+Object.defineProperty(user, "name", {
+  value: "John"
+});
+
+let descriptor = Object.getOwnPropertyDescriptor(user, "name");
+
+alert(JSON.stringify(descriptor, null, 2));
+/*
+{
+  "value": "John",
+  "writable": false,
+  "enumerable": false,
+  "configurable": false
+}
+*/
+
+// ---- ----
+
+// change descriptor
+let user = {
+  name: "John"
+};
+
+Object.defineProperty(user, "name", {
+  writable: false
+});
+
+user.name = "Pete"; // Error: Cannot assign to read only property 'name'
