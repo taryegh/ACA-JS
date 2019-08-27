68c9ad000742e7724725c66ab85d36ae44e71277
taryegh <taryegh@gmail.com>
2019-08-28 00:09:38 +0400

    push

--- Practice/react-practice-code-addict/src/index.js
+++ Practice/react-practice-code-addict/src/index.js
@@ -1,3 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
 
+function Greeting() {
+  return (
+    <div>
+      <h1>hello from React</h1>
+      <h1>it's me</h1>
+    </div>
+  );
+}
+
+// React.createElement syntax - 3 arguments
+// element, propsObject, children
+// converter compiler - Babel
+/* function Greeting() {
+  return React.createElement("div", {}, "hello world");
+}
+
+ReactDOM.render(<Greeting />, document.getElementById("root")); */
