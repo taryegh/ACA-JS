5339cdafeacf694be96ce231c990fae1a7dfeb91
taryegh <taryegh@gmail.com>
2019-08-28 04:13:59 +0400

    book project added

--- Practice/react-practice-code-addict/src/index.js
+++ Practice/react-practice-code-addict/src/index.js
@@ -1,20 +1,64 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import "./index.css";
 
-function Greeting() {
+function Book() {
+  return (
+    <section className="book">
+      <CoverImage />
+      <Title />
+      <Author />
+    </section>
+  );
+}
+
+
+/* // PROJECT - BOOK
+function Book() {
+  return (
+    <section className="book">
+      <CoverImage />
+      <Title />
+      <Author />
+    </section>
+  );
+}
+
+const CoverImage = () => (
+  <img
+    width="250"
+    src="https://m.media-amazon.com/images/I/41T53nRtyoL._AC_UY218_.jpg"
+    alt="book"
+  />
+);
+
+const Title = () => (
+  <h1>Web Design with HTML, CSS, JavaScript and jQuery Set</h1>
+);
+
+const Author = () => (
+  <p style={{ backgroundColor: "black", color: "white" }}>Jon Duckett</p>
+); */
+
+// NESTED COMPONENTS
+/* function Greeting() {
   return (
     <div>
-      <h1>hello from React</h1>
-      <h1>it's me</h1>
+      <Person />
+      <Message />
     </div>
   );
 }
+
+const Person = () => <h1>john</h1>;
+const Message = () => <p>it's a message</p>; */
 
 // React.createElement syntax - 3 arguments
 // element, propsObject, children
 // converter compiler - Babel
 /* function Greeting() {
   return React.createElement("div", {}, "hello world");
-}
+} 
+*/
 
-ReactDOM.render(<Greeting />, document.getElementById("root")); */
+ReactDOM.render(<Book />, document.getElementById("root"));
