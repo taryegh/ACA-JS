6b47a3af4e263f9935aa793a90607e4ffa78ddcd
taryegh <taryegh@gmail.com>
2019-08-29 09:27:16 +0400

    components ready

---
+++ Practice/react-practice-code-addict-2/src/components/Book.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+const Book = () => <h3>hello from Book</h3>;
+
+export default Book;
---
+++ Practice/react-practice-code-addict-2/src/components/Booklist.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Book from "./Book";
+
+const Booklist = () => {
+  return (
+    <div>
+      <h2>hello from booklist</h2>
+      <Book />
+    </div>
+  );
+};
+
+export default Booklist;
