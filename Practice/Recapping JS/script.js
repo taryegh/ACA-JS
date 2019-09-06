ce76d3b10c34154354effa0a100d5104a3b734cb
taryegh <taryegh@gmail.com>
2019-09-06 14:07:56 +0400

    another update

--- Practice/Recapping JS/script.js
+++ Practice/Recapping JS/script.js
@@ -144,7 +144,7 @@ console.log(null === +"\n0\n"); // false
 
 /*
 //////////////////////////////////////////
-// -------- 2.8 INTERACTION --------------
+// -------- 2.9 INTERACTION --------------
 //////////////////////////////////////////
 
 // Question 1
@@ -157,7 +157,7 @@ alert(`Name is ${name}`);
 
 /*
 ///////////////////////////////////////////
-// -------- 2.8 CONDITIONALS --------------
+// -------- 2.10 CONDITIONALS --------------
 ///////////////////////////////////////////
 
 // A number 0, an empty string "", null, undefined, and NaN all become false. Because of that they are called “falsy” values.
@@ -238,4 +238,203 @@ message =
     : "";
 
 */
+
+/*
+/////////////////////////////////////////////////
+// -------- 2.11 LOGICAL OPERATORS --------------
+/////////////////////////////////////////////////
+
+// The OR || operator does the following:
+
+// Evaluates operands from left to right.
+// For each operand, converts it to boolean. If the result is true, stops and returns the original value of that operand.
+// If all operands have been evaluated (i.e. all were false), returns the last operand.
+
+// The AND && operator does the following:
+
+// Evaluates operands from left to right.
+// For each operand, converts it to a boolean. If the result is false, stops and returns the original value of that operand.
+// If all operands have been evaluated (i.e. all were truthy), returns the last operand.
+
+// Precedence of AND && is higher than OR ||
+
+// Question 1
+// What is the code below going to output?
+
+console.log(null || 2 || undefined); // 2
+
+// Question 2
+// What will the code below output?
+
+console.log(console.log(1) || 2 || alert(3)); // 1 then 2 because call of console.log does not return a value
+
+// Question 3
+// What is this code going to show?
+
+alert(1 && null && 2); // null
+
+// Question 4
+// What will the result be?
+
+alert(null || (2 && 3) || 4); // 3
+
+// Question 5
+// Write an “if” condition to check that age is between 14 and 90 inclusively.
+// “Inclusively” means that age can reach the edges 14 or 90.
+
+let age = +prompt("Enter age");
+
+if (age >= 14 && age <= 90) {
+  console.log("age is between 14 and 90 inclusively");
+} else {
+  console.log("not");
+}
+
+// Question 6
+// Write an if condition to check that age is NOT between 14 and 90 inclusively.
+
+// Create two variants: the first one using NOT !, the second one – without it.
+
+// first variant
+let age = +prompt("Enter age");
+
+if (!(age >= 14 && age <= 90)) {
+  console.log("age is NOT between 14 and 90 inclusively.");
+} else {
+  console.log("is");
+}
+
+// second variant
+let age = +prompt("Enter age");
+
+if (age < 14 || age > 90) {
+  console.log("age is NOT between 14 and 90 inclusively.");
+} else {
+  console.log("is");
+}
+
+
+// Question 7
+// Which of these alerts are going to execute?
+// What will the results of the expressions be inside if(...)?
+
+if (-1 || 0) alert( 'first' ); // first
+if (-1 && 0) alert( 'second' );
+if (null || -1 && 1) alert( 'third' ); // third
+
+
+// Question 8
+// Write the code which asks for a login with prompt.
+
+// If the visitor enters "Admin", then prompt for a password, if the input is an empty line or Esc – show “Canceled”, if it’s another string – then show “I don’t know you”.
+
+// The password is checked as follows:
+
+// If it equals “TheMaster”, then show “Welcome!”,
+// Another string – show “Wrong password”,
+// For an empty string or cancelled input, show “Canceled”
+
+let login = prompt("Enter login");
+
+if (login === "Admin") {
+  let password = prompt("Enter password");
+  if (password === "TheMaster") {
+    console.log("Welcome");
+  } else if (password === "" || password === null) {
+    console.log("canceled");
+  } else {
+    console.log("Wrong password");
+  }
+} else if (login === "" || login === null) {
+  console.log("canceled");
+} else {
+  console.log("I don't know");
+}
+
+*/
+
+/*
+/////////////////////////////////////////////////
+// ---------------- 2.12 LOOPS -------------------
+/////////////////////////////////////////////////
+
+// Syntax constructs that are not expressions cannot be used with the ternary operator ?. In particular, directives such as break/continue aren’t allowed there.
+
+// (i > 5) ? alert(i) : continue; // continue isn't allowed here
+
+// instead to use
+
+// if (i > 5) {
+//   alert(i);
+// } else {
+//   continue;
+// }
+
+// Question 1
+// What is the last value alerted by this code? Why?
+
+let i = 3;
+
+while (i) {
+  alert(i--); // 1, because 0 is a falsy value
+}
+
+
+// Question 2
+// For every loop iteration, write down which value it outputs and then compare it with the solution.
+
+// The prefix form ++i:
+
+let i = 0;
+while (++i < 5) console.log(i); // 1, 2, 3, 4
+
+// The postfix form i++
+
+let i = 0;
+while (i++ < 5) console.log(i); // 1, 2, 3, 4, 5
+
+
+
+
+// Question 3
+// For each loop write down which values it is going to show. Then compare with the answer.
+
+// The postfix form:
+
+for (let i = 0; i < 5; i++) console.log(i); // 0, 1, 2, 3, 4
+// The prefix form:
+
+for (let i = 0; i < 5; ++i) console.log(i); // 0, 1, 2, 3, 4,
+
+
+
+// Question 4
+// Use the for loop to output even numbers from 2 to 10.
+for (let i = 2; i <= 10; i++) {
+  if (i % 2 === 0) console.log(i);
+}
+
+
+
+// Question 5
+// Rewrite the code changing the for loop to while without altering its behavior (the output should stay same).
+
+//  for (let i = 0; i < 3; i++) {
+//   alert( `number ${i}!` );
+// }
+
+let i = 0;
+while (i < 3) {
+  console.log(i);
+  i++;
+}
+
+*/
+
+// Question 6
+// Write a loop which prompts for a number greater than 100. If the visitor enters another number – ask them to input again.
+
+// The loop must ask for a number until either the visitor enters a number greater than 100 or cancels the input/enters an empty line.
+
+// Here we can assume that the visitor only inputs numbers. There’s no need to implement a special handling for a non-numeric input in this task.
 
